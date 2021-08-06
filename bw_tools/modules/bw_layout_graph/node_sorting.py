7ce3f5591a65beb0bb635ec1395dd3d5848a43bf
ben-wilson-github <ben.q.wilson@gmail.com>
2021-08-06 15:34:14 +0200

    Fixed bug where offsets where incorrectly calculated

--- bw_tools/modules/bw_layout_graph/node_sorting.py
+++ bw_tools/modules/bw_layout_graph/node_sorting.py
@@ -11,24 +11,26 @@ def run_sort(node: LayoutNode):
     build_alignment_behaviors(node)
 
 
-def position_nodes(node: LayoutNode):
-    for input_node in node.input_nodes:
-        node.set_position(
-            node.closest_output_node_in_x.pos.x
-            - get_offset_value(node, node.closest_output_node_in_x),
-            node.farthest_output_nodes_in_x[0].pos.y,
+def position_nodes(output_node: LayoutNode):
+    input_node: LayoutNode
+    for input_node in output_node.input_nodes:
+        input_node.set_position(
+            input_node.closest_output_node_in_x.pos.x
+            - get_offset_value(input_node, input_node.closest_output_node_in_x),
+            input_node.farthest_output_nodes_in_x[0].pos.y,
         )
         position_nodes(input_node)
 
 
-def build_alignment_behaviors(node: LayoutNode):
-    for input_node in node.input_nodes:
-        if node.alignment_behavior is None:
-            node.alignment_behavior = StaticAlignment(node)
+def build_alignment_behaviors(output_node: LayoutNode):
+    input_node: LayoutNode
+    for input_node in output_node.input_nodes:
+        if input_node.alignment_behavior is None:
+            input_node.alignment_behavior = StaticAlignment(input_node)
 
-        if node.pos.x > node.alignment_behavior.offset_node.pos.x:
-            node.alignment_behavior.offset_node = node
-            node.alignment_behavior.update_offset(node.pos)
+        if output_node.pos.x > input_node.alignment_behavior.offset_node.pos.x:
+            input_node.alignment_behavior.offset_node = output_node
+            input_node.alignment_behavior.update_offset(input_node.pos)
         build_alignment_behaviors(input_node)
 
 
