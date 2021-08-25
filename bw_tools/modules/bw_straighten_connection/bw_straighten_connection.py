5ae6f0a16afec2382758f32bb718a46f16174b8f
ben-wilson-github <ben.q.wilson@gmail.com>
2021-08-25 21:16:48 +0200

    refactor

--- bw_tools/modules/bw_straighten_connection/bw_straighten_connection.py
+++ bw_tools/modules/bw_straighten_connection/bw_straighten_connection.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from dataclasses import dataclass, field
 from bw_tools.common.bw_node import Float2, SDConnection, SDProperty
 from bw_tools.modules.bw_straighten_connection.straighten_behavior import (
     NextToOutput,
@@ -6,7 +7,7 @@ from bw_tools.modules.bw_straighten_connection.straighten_behavior import (
 
 from functools import partial
 from pathlib import Path
-from typing import TYPE_CHECKING, List, Tuple, TypeVar
+from typing import TYPE_CHECKING, List, Tuple, TypeVar, Dict
 
 import sd
 from PySide2 import QtGui
@@ -24,206 +25,24 @@ if TYPE_CHECKING:
 # # TODO: Check if can remove the list copy in remove dot nodes
 
 
-# # def node_is_too_close(
-# #     source_node: sd.api.sdnode.SDNode,
-# #     target_node: sd.api.sdnode.SDNode,
-# #     threshold: float,
-# # ) -> bool:
-# #     if source_node.getPosition().x < target_node.getPosition().x - threshold:
-# #         return False
-# #     else:
-# #         return True
+@dataclass
+class BaseDotNodeBounds:
+    upper_bound: int = 0
+    lower_bound: int = 0
 
 
-# # def sort_connections_by_position_x(
-# #     connections: sd.api.sdconnection.SDConnection,
-# # ) -> Tuple[sd.api.sdconnection.SDConnection]:
-# #     sorted_list = list(connections)
-# #     swapped = True
-# #     while swapped:
-# #         swapped = False
-# #         for i in range(len(sorted_list) - 1):
-# #             if (
-# #                 sorted_list[i].getInputPropertyNode().getPosition().x
-# #                 == sorted_list[i + 1].getInputPropertyNode().getPosition().x
-# #             ):
-# #                 if (
-# #                     sorted_list[i].getInputPropertyNode().getPosition().y
-# #                     == sorted_list[i + 1]
-# #                     .getInputPropertyNode()
-# #                     .getPosition()
-# #                     .y
-# #                 ):
-# #                     if (
-# #                         sorted_list[i].getInputPropertyNode().getIdentifier()
-# #                         > sorted_list[i + 1]
-# #                         .getInputPropertyNode()
-# #                         .getIdentifier()
-# #                     ):
-# #                         sorted_list[i], sorted_list[i + 1] = (
-# #                             sorted_list[i + 1],
-# #                             sorted_list[i],
-# #                         )
-# #                         swapped = True
-
-# #                 elif (
-# #                     sorted_list[i].getInputPropertyNode().getPosition().y
-# #                     > sorted_list[i + 1].getInputPropertyNode().getPosition().y
-# #                 ):
-# #                     sorted_list[i], sorted_list[i + 1] = (
-# #                         sorted_list[i + 1],
-# #                         sorted_list[i],
-# #                     )
-# #                     swapped = True
-# #             elif (
-# #                 sorted_list[i].getInputPropertyNode().getPosition().x
-# #                 > sorted_list[i + 1].getInputPropertyNode().getPosition().x
-# #             ):
-# #                 sorted_list[i], sorted_list[i + 1] = (
-# #                     sorted_list[i + 1],
-# #                     sorted_list[i],
-# #                 )
-# #                 swapped = True
-# #     return tuple(sorted_list)
-
-
-# # def insert_dot_node(
-# #     graph: sd.api.sbs.sdsbscompgraph.SDSBSCompGraph,
-# #     source_node: sd.api.sdnode.SDNode,
-# #     source_property: sd.api.sdproperty.SDProperty,
-# #     target_node: sd.api.sdnode.SDNode,
-# #     target_property: sd.api.sdproperty.SDProperty,
-# #     threshold: float,
-# # ) -> sd.api.sdnode.SDNode:
-# #     dot_node = graph.newNode("sbs::compositing::passthrough")
-
-# #     source_node.newPropertyConnection(
-# #         source_property,
-# #         dot_node,
-# #         dot_node.getPropertyFromId(
-# #             "input", sd.api.sdproperty.SDPropertyCategory.Input
-# #         ),
-# #     )
-
-# #     dot_node.newPropertyConnection(
-# #         dot_node.getPropertyFromId(
-# #             "unique_filter_output", sd.api.sdproperty.SDPropertyCategory.Output
-# #         ),
-# #         target_node,
-# #         target_property,
-# #     )
-
-# #     # position_at_source = get_from_settings_file(
-# #     #     __file__.replace(".py", "_settings.json"), "Position At Source"
-# #     # )
-# #     # if position_at_source:
-# #     #     x = bw_node.Node(source_node).pos.x + threshold
-# #     #     y = bw_node.Node(target_node).y_position_of_property(target_property)
-# #     # else:
-# #     x = StraightenNode(target_node).pos.x - threshold
-# #     y = StraightenNode(source_node).y_position_of_property(source_property)
-
-# #     dot_node.setPosition(sd.api.sdbasetypes.float2(x, y))
-# #     return dot_node
-
-
-# # def is_dot_node(node: SDNode) -> bool:
-# #     return node.getDefinition().getId() == "sbs::compositing::passthrough"
-
-
-# # def get_connections_for_node_property(
-# #     graph: SDSBSCompGraph,
-# #     source_node: SDNode,
-# #     source_node_property: SDProperty,
-# # ) -> List[SDConnection]:
-# #     def already_seen(src, ret):
-# #         for c in ret:
-# #             if (
-# #                 c.getInputPropertyNode().getIdentifier()
-# #                 == src.getInputPropertyNode().getIdentifier()
-# #                 and c.getInputProperty().getId()
-# #                 == src.getInputProperty().getId()
-# #             ):
-# #                 return True
-# #         return False
-
-# #     ret = list()
-# #     for connection in source_node.getPropertyConnections(source_node_property):
-# #         next_node_in_chain = connection.getInputPropertyNode()
-# #         if is_dot_node(next_node_in_chain):
-# #             ret += get_connections_for_node_property(
-# #                 graph,
-# #                 next_node_in_chain,
-# #                 next_node_in_chain.getPropertyFromId(
-# #                     "unique_filter_output",
-# #                     sd.api.sdproperty.SDPropertyCategory.Output,
-# #                 ),
-# #             )
-# #             graph.deleteNode(connection.getInputPropertyNode())
-# #         else:
-# #             if already_seen(connection, ret):
-# #                 continue
-# #             ret.append(connection)
-# #     return ret
-
-
-# # def straighten_connection(
-# #     source_node: SDNode,
-# #     source_node_property: SDProperty,
-# #     api: APITool,
-# # ):
-
-# #     threshold = 96
-
-# #     connections = get_connections_for_node_property(
-# #         api.current_graph, source_node, source_node_property
-# #     )
-# #     sorted_connections = sort_connections_by_position_x(connections)
-
-# #     dot_node = None
-# #     for connection in sorted_connections:
-# #         if dot_node is None:  # On first iteration
-# #             if node_is_too_close(
-# #                 source_node, connection.getInputPropertyNode(), threshold
-# #             ):
-# #                 continue
-# #             else:
-# #                 dot_node = insert_dot_node(
-# #                     api.current_graph,
-# #                     source_node,
-# #                     source_node_property,
-# #                     connection.getInputPropertyNode(),
-# #                     connection.getInputProperty(),
-# #                     threshold,
-# #                 )
-# #         else:
-# #             if node_is_too_close(
-# #                 dot_node,
-# #                 connection.getInputPropertyNode(),
-# #                 threshold + (threshold / 2),
-# #             ):
-# #                 dot_node.newPropertyConnection(
-# #                     dot_node.getPropertyFromId(
-# #                         "unique_filter_output",
-# #                         sd.api.sdproperty.SDPropertyCategory.Output,
-# #                     ),
-# #                     connection.getInputPropertyNode(),
-# #                     connection.getInputProperty(),
-# #                 )
-# #             else:
-# #                 dot_node = insert_dot_node(
-# #                     api.current_graph,
-# #                     dot_node,
-# #                     dot_node.getPropertyFromId(
-# #                         "unique_filter_output",
-# #                         sd.api.sdproperty.SDPropertyCategory.Output,
-# #                     ),
-# #                     connection.getInputPropertyNode(),
-# #                     connection.getInputProperty(),
-# #                     threshold,
-# #                 )
-
-
+@dataclass
+class StraightenConnectionData:
+    connection: Dict[int, SDConnection] = field(default_factory=dict)
+    base_dot_node: Dict[int, StraightenNode] = field(default_factory=dict)
+    current_source_node: Dict[int, StraightenNode] = field(
+        default_factory=dict
+    )
+    stack_index: Dict[int, int] = field(default_factory=dict)
+    properties_with_outputs_count: int = 0
+    base_dot_nodes_bounds: BaseDotNodeBounds = field(
+        default_factory=BaseDotNodeBounds
+    )
 
 
 def run_straighten_connection(
@@ -231,38 +50,60 @@ def run_straighten_connection(
     behavior: AbstractStraightenBehavior,
     api: APITool,
 ):
-    # Insert first dot nodes
+    data = _create_connection_data_for_all_inputs(source_node)
+    _create_base_dot_nodes(source_node, data, behavior)
+    behavior.align_base_dot_nodes(source_node, data)
+
+    _insert_target_dot_nodes(source_node, data, behavior)
+
+    if (
+        behavior.delete_base_dot_nodes
+        and source_node.output_connectable_properties_count
+        == data.properties_with_outputs_count
+    ):
+        _delete_base_dot_nodes(source_node, data)
+
+
+def _create_connection_data_for_all_inputs(
+    source_node: StraightenNode,
+) -> StraightenConnectionData:
+    data = StraightenConnectionData()
+    for i, api_property in enumerate(
+        source_node.output_connectable_properties
+    ):
+        cons = source_node._get_connected_output_connections_for_property(
+            api_property
+        )
+        cons.sort(key=lambda c: c.getInputPropertyNode().getPosition().x)
+        data.connection[i] = cons
+        if cons:
+            data.properties_with_outputs_count += 1
+    return data
+
+
+def _create_base_dot_nodes(
+    source_node: StraightenNode,
+    data: StraightenConnectionData,
+    behavior: AbstractStraightenBehavior,
+):
+    """
+    Creates dot nodes at the base of the source node. The dot nodes
+    are stacked downwards from the source nodes position.y.
+
+    These dot nodes will either be connected or deleted later, depending
+    on if the the resulting connections line up with the output pins
+    of the node.
+
+    Returns the upper and lower bound of the new dot nodes
+    """
+    # Store initial bound values
     upper_y = source_node.pos.y
     lower_y = source_node.pos.y
-    connections: List[SDConnection] = list()
-    base_dot_nodes: List[StraightenNode] = list()
-    target_nodes: List[StraightenNode] = list()
-    connection_data = dict()
-    
-    # Get connections
-    property_with_target_count = 0
-    for i, api_property in enumerate(source_node.output_connectable_properties):
-        cons = (
-            source_node._get_connected_output_connections_for_property(
-                api_property
-            )
-        )
-        cons.sort(
-            key=lambda c: c.getInputPropertyNode().getPosition().x
-        )
-        connection_data[i] = {
-            "connections": cons
-        }
-        if cons:
-            property_with_target_count += 1
-    
-
-    # Create base dot nodes
     index = 0
     for i, _ in enumerate(source_node.output_connectable_properties):
-        if not connection_data[i]["connections"]:
+        if not data.connection[i]:
             continue
-        connection = connection_data[i]["connections"][0]
+        connection = data.connection[i][0]
 
         # Stack the node
         # Create base dot node
@@ -270,74 +111,94 @@ def run_straighten_connection(
             source_node.graph.newNode("sbs::compositing::passthrough"),
             source_node.graph,
         )
-        connection_data[i]['dot_node'] = dot_node
-        connection_data[i]['source_node'] = source_node
+        data.base_dot_node[i] = dot_node
+        data.current_source_node[i] = source_node
 
-        if source_node.output_connectable_properties_count != property_with_target_count:
+        if (
+            source_node.output_connectable_properties_count
+            != data.properties_with_outputs_count
+        ):
             _connect_node(source_node, dot_node, connection)
-            connection_data[i]['source_node'] = dot_node
+            data.current_source_node[i] = dot_node
 
         first_target_node = StraightenNode(
             connection.getInputPropertyNode(), source_node.graph
         )
-        pos = behavior.get_position_first_dot(source_node.pos, first_target_node.pos, index)
+        pos = behavior.get_position_first_dot(
+            source_node.pos, first_target_node.pos, index
+        )
         dot_node.set_position(pos.x, pos.y)
-        upper_y = min(pos.y, upper_y)   # Designer Y axis is inverted
+
+        upper_y = min(pos.y, upper_y)  # Designer Y axis is inverted
         lower_y = max(pos.y, lower_y)
-        connection_data[i]["stack_index"] = index
+        data.stack_index[i] = index
         index += 1
 
+    data.base_dot_nodes_bounds.upper_bound = upper_y
+    data.base_dot_nodes_bounds.lower_bound = lower_y
 
 
-    mid_point = (upper_y + lower_y) / 2
-    offset = source_node.pos.y - mid_point
-    # Position base dot nodes. Move to behavior
+def _should_reconnect_with_previous(
+    previous: StraightenNode,
+    next: StraightenNode,
+    data: StraightenConnectionData,
+    i: int,
+):
+    return (
+        previous is not None
+        and previous.identifier == next.identifier
+        or data.base_dot_node[i].pos.y == next.pos.y
+    )
+
+
+def _insert_target_dot_nodes(
+    source_node: StraightenNode,
+    data: StraightenConnectionData,
+    behavior: AbstractStraightenBehavior,
+):
     for i, _ in enumerate(source_node.output_connectable_properties):
-        if not connection_data[i]["connections"]:
-            continue
-        
-        dot_node: StraightenNode = connection_data[i]["dot_node"]
-        dot_node.set_position(dot_node.pos.x, dot_node.pos.y + offset)
-        connection_data[i]["pos"] = dot_node.pos
-
-    GOT LOGIC WORKING AGAIN. NEED TO ADD TARGET DOT NODE BEING REMOVED IF ITS INLINE
-    THEN DO A BIG REFACTOR
-    
-    # Insert target dot nodes
-    for i, _ in enumerate(source_node.output_connectable_properties):
-        if not connection_data[i]["connections"]:
+        if not data.connection[i]:
             continue
 
-        previous_dot_node: StraightenNode = connection_data[i]['source_node']
         previous_target_node = None
-        # Insert any remaining dot nodes
-        for connection in connection_data[i]["connections"]:
-            target_node = StraightenNode(
+        for connection in data.connection[i]:
+            next_target_node = StraightenNode(
                 connection.getInputPropertyNode(), source_node.graph
             )
-            if previous_target_node is not None and previous_target_node.identifier == target_node.identifier:
-                # reconnect with previous dot node
-                _connect_node(previous_dot_node, target_node, connection)
+            target_dot_pos: Float2 = behavior.get_position_target_dot(
+                data.base_dot_node[i].pos,
+                next_target_node.pos,
+                data.stack_index[i],
+            )
+
+            if _should_reconnect_with_previous(
+                previous_target_node, next_target_node, data, i
+            ):
+                _connect_node(
+                    data.current_source_node[i], next_target_node, connection
+                )
             else:
                 # Create target dot node
                 new_dot_node = StraightenNode(
                     source_node.graph.newNode("sbs::compositing::passthrough"),
                     source_node.graph,
                 )
+                new_dot_node.set_position(target_dot_pos.x, target_dot_pos.y)
+                _connect_node(
+                    data.current_source_node[i], new_dot_node, connection
+                )
+                _connect_node(new_dot_node, next_target_node, connection)
+                data.current_source_node[i] = new_dot_node
+                previous_target_node = next_target_node
 
-                pos = behavior.get_position_dot(connection_data[i]["pos"], target_node.pos, connection_data[i]["stack_index"])
-                new_dot_node.set_position(pos.x, pos.y)
-                _connect_node(previous_dot_node, new_dot_node, connection)
-                _connect_node(new_dot_node, target_node, connection)
-                previous_dot_node = new_dot_node
-                previous_target_node = target_node
-    
-    # remove floating dot nodes if any
-    if source_node.output_connectable_properties_count == property_with_target_count:
-        for i, _ in enumerate(source_node.output_connectable_properties):
-            if not connection_data[i]["connections"]:
-                continue
-            source_node.graph.deleteNode(connection_data[i]['dot_node'].api_node)
+
+def _delete_base_dot_nodes(
+    source_node: StraightenNode, data: StraightenConnectionData
+):
+    for i, _ in enumerate(source_node.output_connectable_properties):
+        if not data.connection[i]:
+            continue
+        source_node.graph.deleteNode(data.base_dot_node[i].api_node)
 
 
 def _connect_node(
--- bw_tools/modules/bw_straighten_connection/straighten_behavior.py
+++ bw_tools/modules/bw_straighten_connection/straighten_behavior.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
+from bw_tools.modules.bw_straighten_connection.bw_straighten_connection import StraightenConnectionData
 from bw_tools.common.bw_node import Float2
 from dataclasses import dataclass
-from typing import TypeVar, TYPE_CHECKING
+from typing import Dict, TypeVar, TYPE_CHECKING
 import sd
 
 if TYPE_CHECKING:
     from .straighten_node import StraightenNode
+    from .bw_straighten_connection import BaseDotNodeBounds
 
 DISTANCE = 96
 STRIDE = 21.33  # Magic number between each input slot
@@ -17,6 +19,11 @@ STRIDE = 21.33  # Magic number between each input slot
 class AbstractStraightenBehavior(ABC):
     graph: None
 
+    @property
+    @abstractmethod
+    def delete_base_dot_nodes(self) -> bool:
+        pass
+
     @abstractmethod
     def get_position_first_dot(
         self,
@@ -27,7 +34,7 @@ class AbstractStraightenBehavior(ABC):
         pass
 
     @abstractmethod
-    def get_position_dot(
+    def get_position_target_dot(
         self,
         dot_node: StraightenNode,
         source_pos: Float2,
@@ -36,23 +43,43 @@ class AbstractStraightenBehavior(ABC):
     ) -> Float2:
         pass
 
+    @abstractmethod
+    def align_base_dot_nodes(self, source_node: StraightenNode, data: StraightenConnectionData):
+        pass
+
+
 
 class NextToOutput(AbstractStraightenBehavior):
+    @property
+    def delete_base_dot_nodes(self) -> bool:
+        return True
+
     def get_position_first_dot(
         self,
         source_pos: Float2,
         target_pos: Float2,
         index: int,
-    ):
+    ) -> Float2:
         return Float2(source_pos.x + DISTANCE, source_pos.y + (STRIDE * index))
 
-    def get_position_dot(
+    def get_position_target_dot(
         self,
         source_pos: Float2,
         target_pos: Float2,
         index: int,
-    ):
+    ) -> Float2:
         return Float2(target_pos.x - DISTANCE, source_pos.y)
+    
+    def align_base_dot_nodes(self, source_node: StraightenNode, data: StraightenConnectionData):
+        mid_point = (data.base_dot_nodes_bounds.upper_bound + data.base_dot_nodes_bounds.lower_bound) / 2
+        offset = source_node.pos.y - mid_point
+        # Position base dot nodes. Move to behavior
+        for i, _ in enumerate(source_node.output_connectable_properties):
+            if not data.connection[i]:
+                continue
+
+            dot_node: StraightenNode = data.base_dot_node[i]
+            dot_node.set_position(dot_node.pos.x, dot_node.pos.y + offset)
 
 
 class NextToInput(AbstractStraightenBehavior):
