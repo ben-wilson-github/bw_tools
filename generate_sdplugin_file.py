1d92dc022a67b08bb7bf95031e3171741d0d1a13
ben-wilson-github <75605131+ben-wilson-github@users.noreply.github.com>
2021-10-21 19:46:29 +0200

    Dev (#13)
    * set settings action to NoRole for MacOS

--- generate_sdplugin_file.py
+++ generate_sdplugin_file.py
@@ -7,7 +7,7 @@ import os
 
 def main():
     plugin_name = "bw_tools"
-    version = "2.0.7"
+    version = "2.0.1"
     min_designer_version = "11.2.0"
     author = "Ben Wilson"
     email = "ben.q.wilson@gmail.com"
